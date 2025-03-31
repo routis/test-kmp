1040946297c0297fac67614834b16052b4a78658
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2025-03-31 16:37:15 +0300

    Remove unneeded targets.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,5 +1,4 @@
 import com.vanniktech.maven.publish.SonatypeHost
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -15,16 +14,10 @@ kotlin {
     jvm()
     androidTarget {
         publishLibraryVariants("release")
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-    linuxX64()
-
     sourceSets {
         val commonMain by getting {
             dependencies {
